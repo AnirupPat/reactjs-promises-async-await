9fb3827a3328c1f13baae332a157eeac8a2f4505
Anirup Patnaik <anirup049@gmail.com>
2021-06-17 16:07:13 +0530

    Returning Promises

--- src/methods/ReturningPromises.js
+++ src/methods/ReturningPromises.js
@@ -1,5 +1,28 @@
 const ReturningPromises = () => {
-
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(1);
+        }, 1000);
+    });
+    promise.then(
+        result => {
+            alert(result);
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(result * 2);
+                }, 1000);
+            })
+        }
+    ).then(
+        result => {
+            alert(result);
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(result * 2);
+                }, 1000);
+            })
+        }
+    );
 }
 
 export default ReturningPromises;
