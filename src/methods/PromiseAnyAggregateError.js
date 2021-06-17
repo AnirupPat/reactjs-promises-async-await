44470cc6652448b85c411b4b9c68f4fa6d9240e1
Anirup Patnaik <anirup049@gmail.com>
2021-06-18 00:16:35 +0530

    Aggregate Error in Promise Any

--- src/methods/PromiseAnyAggregateError.js
+++ src/methods/PromiseAnyAggregateError.js
@@ -1,5 +1,12 @@
 const PromiseAnyAggregateError = () => {
-
+    Promise.any([
+        new Promise((resolve, reject) => setTimeout(() => reject(new Error('Error 1')), 1000)),
+        new Promise((resolve, reject) => setTimeout(() => reject(new Error('Error 2')), 2000))
+    ]).catch(error => {
+        alert(error.constructor.name); // AggregateError
+        alert(error.errors[0]); // Error: Ouch!
+        alert(error.errors[1]); // Error: Error
+    })
 }
 
 export default PromiseAnyAggregateError;
